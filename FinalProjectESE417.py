97ebea3ee96f4a440ca95de5a5e7dddd133c4560
Alexander Flynn <aflynn0213@gmail.com>
2021-05-07 17:32:08 -0500

    LOOKIN GOOD

--- FinalProjectESE417.py
+++ FinalProjectESE417.py
@@ -14,12 +14,11 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.naive_bayes import GaussianNB
 import seaborn as sns
 from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix
 
 
 def data_normalization(wine):
     for i in range(6):
-        print(i)
-        print(wine)
         var = np.var(wine[:,i])
         mean = np.sum(wine[:,i])/len(wine[:,i])
         wine[:,i] = (wine[:,i]-mean)/var
@@ -34,27 +33,27 @@ while(True):
     if (choice == 5):
         break
 
-    bin = 0
-    while (bin != 1 and bin != 2):
-        bin = int(input("DO YOU WANT\n1)BINARY\n2)MULTI-CLASSIFICATION\n\n"))
+    #bin = 0
+    #while (bin != 1 and bin != 2):
+    #    bin = int(input("DO YOU WANT\n1)BINARY\n2)MULTI-CLASSIFICATION\n\n"))
 
-    bin = True if(bin == 1) else False
+    #bin = True if(bin == 1) else False
+
     #LOAD DATA FROM CSV
     wine_data = pd.read_csv('winequality-red.csv', sep=",")
 
     #SEPERATE INTO INPUTS (X) AND CLASS LABEL (Y)
     X = wine_data.loc[:,"fixed acidity":"alcohol"]
 
-    if (bin):
-        for i in range(1599):
-            print(i)
-            if wine_data.iloc[i, -1] > 6:
-                wine_data.iloc[i, -1] = 1
-            else:
-                wine_data.iloc[i,-1] = 0
+    #if (bin):
+    #    for i in range(1599):
+    #        if wine_data.iloc[i, -1] > 6:
+    #            wine_data.iloc[i, -1] = 1
+    #        else:
+    #            wine_data.iloc[i,-1] = 0
         #print(wine_data)
-
     Y = wine_data.loc[:,"quality"]
+
     #SPLIT INTO TRAINING AND TEST DATA
     tr_x, te_x, tr_y, te_y = train_test_split(X, Y, test_size=0.25, random_state=5)
     #SELECTING 'K' BEST FEATURES BASED OFF OF CHI2 SCORE
@@ -91,7 +90,7 @@ while(True):
     #plt.show()
 
     #Plotting histogram of each variable
-    plt.figure(1)
+    plt.figure()
     wine_data.hist(alpha=0.5, figsize=(15, 10))
     plt.tight_layout()
     plt.show()
@@ -99,42 +98,85 @@ while(True):
     if (choice==1):
 
         print("ARTIFICIAL NEURAL NETWORK\n\n")
+        layers = [(10,10),(25,25),(50,50),(100,100),(250,250)]
 
         #HYPERTUNING PARAMETERS USING GRID SEARCH METHOD AND MLP CLASSIFIER
-        pr_grid = {'hidden_layer_sizes':[(10,10),(25,25),(50,50),(100,100),(250,250)],
-                    'learning_rate_init' : [.001,.01]}
+        pr_grid = {'hidden_layer_sizes': layers,
+                    'learning_rate_init' : [.001,.01],
+                    'activation' : ['tanh','logistic']}
+
         ann = MLPClassifier(max_iter = 500, random_state=420, early_stopping = True)
         cv_ann = GridSearchCV(ann,param_grid=pr_grid)
         cv_ann.fit(tr_x, tr_y)
         #PRINT OUT BEST PARAMETERS AND THE SCORE THEY PROVIDED
         print("The best parameters are %s with a score of %0.5f" % (cv_ann.best_params_, cv_ann.best_score_))
         #TEST SCORE
-        print("GRID SCORE FOR TEST DATA: "+str(cv_ann.score(te_x,te_y)*100))
+        print("GRID SCORE FOR TEST DATA: %0.5f" % cv_ann.score(te_x,te_y))
 
-
-        #BEST PARAMETERS FOUND FROM HYPERTUNING
-        MLP = MLPClassifier(activation = 'tanh',
-                           solver = 'adam',
-                           hidden_layer_sizes = cv_ann.best_params_['hidden_layer_sizes'],
-                           max_iter = 500,
-                           random_state = 420,
-                           early_stopping = True,
-                           learning_rate_init = cv_ann.best_params_['learning_rate_init'])
-        MLP.fit(tr_x,tr_y)
-        score_train.append(MLP.score(tr_x,tr_y))
-        score_test.append(MLP.score(te_x,te_y))
-        y_pred = MLP.predict(te_x)
-
+        #USE BEST PARAMATERS TO TRAIN SKLEARN MODEL THEN PLOT CONFUSION MATRIX
+        mlp = MLPClassifier(activation = cv_ann.best_params_['activation'],
+                   solver = 'adam',
+                   hidden_layer_sizes = cv_ann.best_params_['hidden_layer_sizes'],
+                   max_iter = 500,
+                   random_state = 420,
+                   early_stopping = True,
+                   learning_rate_init = cv_ann.best_params_['learning_rate_init'])
+        mlp.fit(tr_x,tr_y)
+        y_pred = mlp.predict(te_x)
+        score_train = mlp.score(tr_x,tr_y)
+        score_test = mlp.score(te_x,te_y)
         print("Mean Accuracy Score on Training Data: ",score_train)
         print("Mean Accuracy Score on Test Data: ",score_test)
-        print("Accuracy from Metrics Accuracy on Y Predicted vs Y-Test: ",metrics.accuracy_score(te_y,y_pred))
 
-        plt.figure(2)
-        plt.plot(score_train,label = 'train set')
-        plt.plot(score_test,label = 'test set')
-        plt.ylabel('score')
+        #CONFUSION MATRIX PLOTTING
+        plt.figure()
+        cm = confusion_matrix(te_y,y_pred)
+        cm = pd.DataFrame(cm)
+        sns.heatmap(cm,annot=True,annot_kws={"size": 16})
+        plt.show()
+
+        #BEST PARAMETERS FOUND FROM HYPERTUNING
+        layers_index = []
+        score_train = []
+        score_test = []
+
+        for i in ['tanh','logistic']:
+            score_test_sub = []
+            score_train_sub = []
+
+            for j in layers:
+                MLP = MLPClassifier(activation = i, solver = 'adam',
+                                    hidden_layer_sizes = j, max_iter = 500,
+                                    random_state = 420, early_stopping = True,
+                                    learning_rate_init = cv_ann.best_params_['learning_rate_init'])
+                MLP.fit(tr_x,tr_y)
+                score_train_sub.append(MLP.score(tr_x,tr_y))
+                score_test_sub.append(MLP.score(te_x,te_y))
+                if (i == 'tanh'):
+                    layers_index.append(j[0][0])
+            score_train.append(score_test_sub)
+            score_test.append(score_test_sub)
+
+        plt.figure()
+        plt.subplot(211)
+        plt.plot(layers_index,score_test[0])
+        plt.plot(layers_index,score_train[0])
+        plt.ylabel('Score with Tanh Activation')
+        plt.xlabel('# of Nodes in each Hidden Layer')
+        plt.subplot(212)
+        plt.plot(layers_index,score_test[1])
+        plt.plot(layers_index,score_train[1])
+        plt.ylabel('Score with Logistic Activation')
+        plt.xlabel('# of Nodes in each Hidden Layer')
         plt.legend()
         plt.show()
+
+
+
+
+
+
+
 
     elif(choice==2):
 
@@ -153,8 +195,8 @@ while(True):
 
 
         #BEST PARAMETERS FOUND FROM HYPERTUNING
-        giggity = 331
         for i in estimators:
+
             RFC = RandomForestClassifier(n_estimators=i,
                                         random_state=1,
                                         criterion='entropy',
@@ -162,21 +204,14 @@ while(True):
             RFC.fit(tr_x,tr_y)
             score_train.append(RFC.score(tr_x,tr_y))
             score_test.append(RFC.score(te_x,te_y))
-            y_pred = RFC.predict(te_x)
+
 
             print("Mean Accuracy Score on Training Data: ",score_train)
             print("Mean Accuracy Score on Test Data: ",score_test)
-            print("Accuracy from Metrics Accuracy on Y Predicted vs Y-Test: ",metrics.accuracy_score(te_y,y_pred))
 
-            plt.figure()
-            plt.subplot(giggity)
-            plt.plot(score_train,'.',label = 'train set')
-            plt.plot(score_test,'-',label = 'test set')
-            plt.ylabel('score')
-            plt.legend()
-            plt.show()
-
-            giggity += 1
+        plt.figure()
+        plt.plot(n_estimators,score_train,label = 'train set')
+        plt.plot(n_estimators,score_test,label = 'test set')
 
     elif(choice==3):
 
@@ -184,16 +219,23 @@ while(True):
 
         nbc = GaussianNB()
         nbc.fit(tr_x,tr_y)
-        score_train.append(nbc.score(tr_x,tr_y))
-        score_test.append(nbc.score(te_x,te_y))
+        score_train = nbc.score(tr_x,tr_y)
+        score_test = nbc.score(te_x,te_y)
         y_pred = nbc.predict(te_x)
 
         print("Mean Accuracy Score on Training Data: ",score_train)
         print("Mean Accuracy Score on Test Data: ",score_test)
-        print("Accuracy from Metrics Accuracy on Y Predicted vs Y-Test: ",metrics.accuracy_score(te_y,y_pred))
+        #print("Accuracy from Metrics Accuracy on Y Predicted vs Y-Test: ",metrics.accuracy_score(te_y,y_pred))
+
+
+        cm = confusion_matrix(te_y,y_pred)
+        cm = pd.DataFrame(cm)
+        sns.heatmap(cm,annot=True,annot_kws={"size": 16})
+        plt.show()
 
     elif(choice==4):
-        print("SUPPORT VECTOR MACHINE BABY!\n\n")
+
+        print("SUPPORT VECTOR MACHINE\n\n")
 
         kernels = ['linear','poly','rbf','sigmoid']
         cs = [1, 2, 5, 10, 50, 100]
@@ -222,11 +264,11 @@ while(True):
 
             score_train.append(score_train_sub)
             score_test.append(score_test_sub)
-            plt.figure(1)
+            plt.figure()
             plt.subplot(sub_num)
             plt.plot(cs,score_train_sub,label = 'train set')
             plt.plot(cs,score_test_sub,label = 'test set')
-            plt.ylabel('Score for Kernel: '+str(i)+'for C Values')
+            plt.ylabel("Score for Kernel: %s for C Values" % i)
             plt.legend()
             sub_num += 1
         plt.show()
